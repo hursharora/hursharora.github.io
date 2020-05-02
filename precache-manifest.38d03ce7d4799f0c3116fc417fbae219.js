3a8023464b1ce38daa5260aee58a5bb0dbb2aa71
Hursh <hursharora@gmail.com>
2020-05-01 17:28:32 -0700

    Updates

---
+++ precache-manifest.38d03ce7d4799f0c3116fc417fbae219.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250fa5be897dd273678a91e9d0e188ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d797f78d5d691fac8e5c",
+    "url": "/static/css/main.fc87dbbe.chunk.css"
+  },
+  {
+    "revision": "ece38fe04b2b64080718",
+    "url": "/static/js/2.91fe5384.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.91fe5384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d797f78d5d691fac8e5c",
+    "url": "/static/js/main.4a191809.chunk.js"
+  },
+  {
+    "revision": "b3fa793bd4d590f92710",
+    "url": "/static/js/runtime-main.ed36117f.js"
+  },
+  {
+    "revision": "5afdcdb91eeee55428b106e2d482a746",
+    "url": "/static/media/DoorIDThumb.5afdcdb9.PNG"
+  },
+  {
+    "revision": "cd01b2a62b635b0705b748e26dfc54d6",
+    "url": "/static/media/TVShowTrackerImg.cd01b2a6.PNG"
+  },
+  {
+    "revision": "232fa4e8e119ed1b45973fbbe3bb7d74",
+    "url": "/static/media/VideoBackground.232fa4e8.mp4"
+  },
+  {
+    "revision": "96620795bd9a8b133962811ce2f08794",
+    "url": "/static/media/headshot.96620795.jpg"
+  },
+  {
+    "revision": "79b7f306413e7b850908cf5a1088024c",
+    "url": "/static/media/logo.79b7f306.png"
+  }
+]);
--- src/SectionHeader/SectionHeader.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import classes from "./SectionHeader.module.css"
-
-
-const SectionHeader = props => (
-    <h1 className={classes.SectionHeader}>{props.children}</h1>
-)
-
-export default SectionHeader;
